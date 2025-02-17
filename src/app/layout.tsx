5de8dcd7f533b52fbaba8a0cb265a486838c7f21
lukaabramishvili <92173962+lukaabramishvili@users.noreply.github.com>
2025-02-17 18:07:20 +0400

    feat(header): Create the basic header structure and add the favicon and other pages (these pages are empty for now)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Header from "./components/Header/header";
+import Footer from "./components/Footer/footer";
 import "./globals.css";
 
 export default function RootLayout({
@@ -8,7 +10,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <Header />
+          {children}
+        <Footer />
       </body>
     </html>
   );
